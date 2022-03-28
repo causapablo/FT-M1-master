3bd93177a288dbf9c70d41fc813c47e7ff371d91
Pablo Augusto Causa <44151017+causapablo@users.noreply.github.com>
2022-03-28 12:29:38 -0300

    Add files via upload

---
+++ 00-IntroToCS/homework/homework.js
@@ -0,0 +1,18 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+  let arrayStringNum = num.toString().split();
+
+}
+
+function DecimalABinario(num) {
+  // tu codigo aca
+
+}
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
